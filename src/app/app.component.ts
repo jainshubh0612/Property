b0bb2341a5f0e3dfd6069972f76837ab0c1b8947
Shubh Jain <shubhjain56854@gmail.com>
2022-08-03 21:20:24 +0530

    task 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Property {
+  name: string;
+  description: string;
+  size: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'MyApp';
+export class AppComponent implements OnInit  {
+  properties: Property[] = [];
+  property: Property = {} as Property;
+
+  ngOnInit(): void {}
+
+  addProperty() {
+    if(this.editIndex != -1) {
+      this.properties[this.editIndex] = { ...this.property };
+    } else {
+      this.properties.push(this.property);
+    }
+
+    this.editIndex = -1;
+    this.property = {} as Property;
+  }
+
+  editIndex: number = -1;
+  editProperty(index: number) {
+    this.editIndex = index;
+
+    this.property = { ...this.properties[index] };
+  }
+
+  deleteProperty(index: number) {
+    this.properties.splice(index, 1);
+  }
 }
